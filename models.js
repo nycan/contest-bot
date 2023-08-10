aa65f7537c20810c707c28a30e8e878d94597ce4
nycan <ny.saskcan@gmail.com>
2023-08-10 15:18:06 -0600

    Added capability for whitelists and blacklists

--- models.js
+++ models.js
@@ -1,5 +1,8 @@
 exports.Contest = class Contest{
-    constructor(code, name, problems, rules, windowStart, windowEnd, duration, problemFiles, solutionFiles){
+    constructor(code, name, problems, rules,
+        windowStart, windowEnd, duration,
+        problemFiles, solutionFiles,
+        whitelist, list){
         this.code = code;
         this.name = name;
         this.problems = problems;
@@ -7,5 +10,9 @@ exports.Contest = class Contest{
         this.windowStart = windowStart;
         this.windowEnd = windowEnd;
         this.duration = duration; // in minutes
+        this.problemFiles = problemFiles;
+        this.solutionFiles = solutionFiles;
+        this.whitelist = whitelist; // boolean. true if whitelist, false if blacklist
+        this.list = list; // set of users
     }
 }
