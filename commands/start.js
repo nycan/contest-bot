a156911bf73e133e90392b4cb80adcb98ce62c8c
nycan <ny.saskcan@gmail.com>
2023-08-10 14:15:44 -0600

    Added contest class that is linked to the start command

--- commands/start.js
+++ commands/start.js
@@ -1,9 +1,28 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('start').setDescription('Write a given contest.'),
+    data: new SlashCommandBuilder().setName('start').setDescription('Write a given contest.')
+    .addStringOption(option =>option.setName('name').setDescription('The code given for the contest.')),
     async execute(interaction) {
-        interaction.user.send("test");
-        await interaction.reply('Instructions have been sent to your DMs.');
+        const contestsPath = path.join(__dirname, '..','contests');
+        const contestsFiles = fs.readdirSync(contestsPath).filter(file => file.endsWith('.js'));
+        var contestParam = null;
+        for(const file of contestsFiles){
+            const filePath = path.join(contestsPath,file);
+            const contest = require(filePath);
+            if(contest.code == interaction.options.getString('name')){
+                contestParam = contest;
+                break;
+            }
+        }
+        if(contestParam == null){
+            await interaction.reply('Invalid contest code.');
+            return;
+        } else {
+            await interaction.user.send(contestParam.name + '\n' + contestParam.rules);
+            await interaction.reply('Instructions have been sent to your DMs.');
+        }
     },
 }
---
+++ contests/test1.js
@@ -0,0 +1,3 @@
+const { Contest } = require('../models.js');
+
+module.exports = new Contest("tc1", "Test contest", [], "This is a test disclaimer.", new Date(2021, 7, 1, 0, 0, 0), new Date(2021, 7, 1, 0, 0, 0), 0, [], []);
