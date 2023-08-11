9ff2148ae39f1f6a26fb51fb7f9fcb12e277dbf1
nycan <ny.saskcan@gmail.com>
2023-08-10 22:09:10 -0600

    Add command to view submissions

--- commands/submissions.js
+++ commands/submissions.js
@@ -1,12 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 
 module.exports = {
     data: new SlashCommandBuilder().setName('submisions').setDescription('View your current submissions.'),
     async execute(interaction) {
-        id = interaction.options.getString('id');
-        answer = interaction.options.getString('Answer');
         if(interaction.channel.type != 1) {
             await interaction.reply('Please use this command in DMs.');
             return;
@@ -22,6 +20,18 @@ module.exports = {
             await interaction.reply('You are not in a contest.');
             return;
         }
-        
+        fields = [];
+        for(let i = 0; i < userParam.answers.length; ++i){
+            fields.push({
+                name: 'Problem ' + (i+1),
+                value: String(userParam.answers[i]),
+            });
+        }
+        const embed = {
+            title: 'Your Submissions',
+            fields: fields,
+            color: 0xd10a0a,
+        };
+        await interaction.reply({embeds: [embed]});
     },
 }
