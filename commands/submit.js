1f9bc321db199c829422b02a6555f93627deb656
nycan <ny.saskcan@gmail.com>
2023-08-10 21:34:53 -0600

    Added time, submit commands

--- commands/submit.js
+++ commands/submit.js
@@ -1,8 +1,42 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('template').setDescription('template'),
+    data: new SlashCommandBuilder().setName('submit').setDescription('Submit an answer.')
+    .addStringOption(option =>option.setName('id').setDescription('The problem number.').setRequired(true))
+    .addStringOption(option =>option.setName('answer').setDescription('Your answer to the problem.').setRequired(true)),
     async execute(interaction) {
-        await interaction.reply('template');
+        id = interaction.options.getString('id');
+        answer = interaction.options.getString('Answer');
+        if(interaction.channel.type != 1) {
+            await interaction.reply('Please use this command in DMs.');
+            return;
+        }
+        const userFile = path.join(__dirname, '..', 'users', interaction.user.id+'.json');
+        let userParam = {};
+        if(fs.existsSync(userFile)){
+            userParam = require(userFile);
+        } else {
+            await interaction.reply('You are not in a contest.');
+        }
+        if(userParam.currContest == ''){
+            await interaction.reply('You are not in a contest.');
+            return;
+        }
+        const contestFile = path.join(__dirname, '..','contests',userParam.currContest+'.json');
+        const contestParam = require(contestFile);
+        if(isNaN(id)){
+            await interaction.reply('Invalid problem number.');
+            return;
+        }
+        id = parseInt(id);
+        if(id < 1 || id > contestParam.problems.length){
+            await interaction.reply('Invalid problem number.');
+            return;
+        }
+        await interaction.reply({content: 'Your answer has been submitted.', ephemeral: true});
+        userParam.answers[id-1] = answer;
+        fs.writeFileSync(userFile, JSON.stringify(userParam));
     },
 }
